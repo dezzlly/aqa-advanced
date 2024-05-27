14a6964ae81090f1e4198c344764fec3d878ed48
dezzly <dezzly21@gmail.com>
2024-05-27 22:55:33 +0300

    Added folder jsAdvancedFunctions and files: task1.js, task2.js, task3.js

---
+++ jsAdvancedFunctions/task1.js
@@ -0,0 +1,15 @@
+const handleEven = () => console.log('Number is even')
+const handleOdd = () => console.log('Number is odd')
+
+
+const handleNum = (num, callback, callback1) => {
+
+    if (num % 2 === 0) {
+        callback()
+        return
+    }
+    callback1()
+}
+
+handleNum(3, handleEven, handleOdd)
+handleNum(8, handleEven, handleOdd)
